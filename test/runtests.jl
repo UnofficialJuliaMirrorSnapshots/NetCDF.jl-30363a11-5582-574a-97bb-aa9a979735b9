c7cca38c9d66fca13d638ce8fdb8f636dfbcf7be
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 00:45:34 +0000

    Snapshot of branch master taken on 2019-04-07T20:45:33.393-04:00 from "https://github.com/UnofficialJuliaMirror/NetCDF.jl-30363a11-5582-574a-97bb-aa9a979735b9"

---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using Test, NetCDF, Random
+
+"Function that returns a valid file name of a non-existing file for all OSs"
+function tempname2()
+    f, h = mktemp()
+    close(h)
+    rm(f)
+    f
+end
+
+include("intermediate.jl")
+include("high.jl")
+include("array-like.jl")
+include("chunks.jl")
